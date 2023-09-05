1ebd34e9f07f45139316b3c5d78904e1df9926ba
aryan0134 <aryan0134.be20@chitkara.edu.in>
2023-09-05 12:09:39 +0530

    asdasd

--- src/pages/ToDoList.js
+++ src/pages/ToDoList.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import workInProgress from '../images/workInProgress.gif'
+
 
 function ToDoList() {
   return (
-    <div>ToDoList</div>
+    <div className='HomePage'>
+        <div className='WorkInProgress'>
+            <img src={workInProgress} />
+                <div className='title'>
+                <h1>Hey! <br></br> <br></br>This Page is under Construction... </h1>
+                </div>
+        </div>
+    </div>
   )
 }
 
