f435816c88fc02c1c1087cfe75694bea585b2cb3
aryan0134 <aryan0134.be20@chitkara.edu.in>
2023-09-17 19:17:49 +0530

    asdasd

--- src/pages/ToDoList.js
+++ src/pages/ToDoList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import workInProgress from '../images/workInProgress.gif'
 import { useUserContext } from "../hooks/useUserContext"
 import todoimg from '../images/todoimg.gif'
@@ -8,19 +8,25 @@ import { useNavigate } from 'react-router-dom'
 function ToDoList() {
   const [value, setValue] = useState("")
   const { dispatch } = useUserContext()
+  const [counter, setCounter] = useState(0)
 
   const navigate = useNavigate()
 
   const handleStatusChange = async (e) => {
     const newStatus = e.target.value;
     setValue(newStatus);
+    setCounter(counter=>counter+1)
+    
+    // dispatch({type: 'SET_USER', payload: value})
+    // navigate('/todomain')
+  }
+
+  useEffect(() => {
     if (value){
       dispatch({type: 'SET_USER', payload: value})
       console.log( value, "user is online")
     }
-    // dispatch({type: 'SET_USER', payload: value})
-    // navigate('/todomain')
-  }
+  }, [counter]);
 
   return (
     <div className='HomePage'>
