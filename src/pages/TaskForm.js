0b75ae6a829941f9428c0a4618a4759223a1aac3
aryan0134 <aryan0134.be20@chitkara.edu.in>
2023-09-19 12:18:48 +0530

    sdad

--- src/pages/TaskForm.js
+++ src/pages/TaskForm.js
@@ -66,7 +66,8 @@ const handleClick = async (arrow) => {
   }
 
 }
-const deletePractice = (arrow) => {
+const deletePractice = (e,arrow) => {
+  e.preventDefault()
   const confirmBox = window.confirm(
     "Do you really want to delete "
   )
@@ -82,7 +83,7 @@ const deletePractice = (arrow) => {
           <div className='task-details-container'>
             <div className='task1'>
               <div className='task1-name'><p>{arrow.taskName}</p></div>
-              <div className='task1-edit'><div><BiEdit /></div> <div onClick={deletePractice(arrow)}><AiFillDelete /></div></div>
+              <div className='task1-edit'><div><BiEdit /></div> <div onClick={deletePractice(e,arrow)}><AiFillDelete /></div></div>
             </div>
             <div className='task2'>
               <div className='task2-division'><h2>{arrow.assignee}</h2></div>
