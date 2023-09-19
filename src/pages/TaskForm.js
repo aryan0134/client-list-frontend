bc1be7efc57efdd81630d3c64a97be8d47722f58
aryan0134 <aryan0134.be20@chitkara.edu.in>
2023-09-19 13:28:04 +0530

    asdasd

--- src/pages/TaskForm.js
+++ src/pages/TaskForm.js
@@ -82,7 +82,7 @@ const deletePractice = (arrow) => {
           <div className='task-details-container'>
             <div className='task1'>
               <div className='task1-name'><p>{arrow.taskName}</p></div>
-              <div className='task1-edit'><div><BiEdit /></div> <div style={{color: "red"}} onClick={()=>{deletePractice(arrow);}}><AiFillDelete /></div></div>
+              <div className='task1-edit'><div><BiEdit /></div> <div style={{color: "red", cursor : "pointer"}} onClick={()=>{deletePractice(arrow);}}><AiFillDelete /></div></div>
             </div>
             <div className='task2'>
               <div className='task2-division'><h2>{arrow.assignee}</h2></div>
